be9a97fb543345506e0c924435484d1240ad951f
Stephanie Tam <steph.mtam@gmail.com>
2021-07-15 22:04:31 -0700

    Initial Commit

---
+++ mini project 2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mini project 2
+//
+//  Created by Tam on 7/15/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
